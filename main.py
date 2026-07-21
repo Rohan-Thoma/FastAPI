7cd6944bffe681747731539b0cff543e3ca7da3f
Rohan-Thoma <rohanvailalathoma@gmail.com>
2026-07-21 17:10:09 +0000

    converted the app to a patient storing app interface etc and defined a view function to fetch patient records

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {'message': "Hello world" }
+    return {'message': "Patient Management System API" }
 
-@app.get("/ about")
+@app.get("/about")
 def about():
-    return {"This is the dummy to test fastapi app"}
+    return {"message" : "A fully functional API to manage your patient records"}
+
+#Helper function to fetch all the patient records from the file in the server
+def load_data():
+    with open("patients.json", 'r') as f:
+        data = json.loads(f)
+    
+    return data 
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
