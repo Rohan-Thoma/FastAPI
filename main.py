106e046ab9699852f74abf4ec6b4646d7777d0b7
Rohan-Thoma <rohanvailalathoma@gmail.com>
2026-07-21 18:02:42 +0000

    http request codes and exception handling is done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -14,7 +14,7 @@ def about():
 #Helper function to fetch all the patient records from the file in the server
 def load_data():
     with open("patients.json", 'r') as f:
-        data = json.loads(f)
+        data = json.load(f)
     
     return data 
 
@@ -24,3 +24,11 @@ def view():
 
     return data
 
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str = Path(..., description="ID of the patient in the Database", example="P001") ):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404, detail="patient not found")
